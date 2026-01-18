5c036cf604839cdd16ff5a47aab66a32a8348ad1
sybille <syb.ndt@gmail.com>
2026-01-18 22:34:09 +0100

    devoir dimanche 18 janvier

---
+++ binary_tree.cpp
@@ -0,0 +1,360 @@
+/*
+Nous souhaitons implémenter un arbre binaire.
+
+Un arbre binaire est formé de noeuds (Node).
+
+Chaque noeud a deux sous-arbres (left, right) potentiellement nuls et une valeur entière.
+
+Un de ces noeuds est la racine de l'arbre, tous les noeuds de l'arbre sont accessibles à partir de cette racine.
+
+Voici un exemple d'arbre avec 3 noeuds,
+on le dessine "allongé" avec sa racine à gauche:
+
+      37
+26
+      11
+
+   la racine est 26
+   l'arbre gauche est réduit au noeud 11
+   l'arbre droit est réduit au noeud 37
+
+Un autre avec les mêmes valeurs
+mais une autre forme (la forme dépend de l'ordre d'insertion des éléments)
+            37
+      26
+11
+
+Vous l'avez compris, étant donné un noeud:
+   - les valeurs des noeuds du sous-arbre gauche sont strictement inférieures à la valeur du noeud
+   - les valeurs des noeuds du sous-arbre droit sont strictement supérieures à la valeur du noeud.
+
+Voici le dessin de l'arbre nous allons prendre en exemple dans le main (8 est la racine):
+en trichant on a inséré les valeurs de manière à ce qu'il soit équilibré...
+Quel est la complexité en temps de la recherche d'un élément dans un arbre binaire équilibré ?
+dégénéré
+               15
+          14
+               13
+     12
+               11
+          10
+               9
+8
+               7
+          6
+               5
+     4
+               3
+          2
+               1
+
+Si j'ai beaucoup moins de chance, je peux obtenir en insérant les mêmes valeurs
+un arbre "dégénéré":
+30
+     29
+          28
+               27
+                    26
+                         25
+                              24
+                                   23
+                                        22
+                                             21
+                                                  20
+                                                       19
+                                                            18
+                                                                 17
+                                                                      16
+
+Quel est la complexité en temps de la recherche d'un élément dans des arbres binaires de cette forme ?
+
+I) Construction de l'arbre:
+
+L'arbre est construit de manière à être trié c'est à dire quand on est à un noeud:
+   - toutes les valeurs de son sous-arbre gauche sont strictement inférieures à la valeur du noeud
+   - toutes les valeurs de son sous-arbre droit sont strictement supérieures à la valeur du noeud
+
+On donne des indices mais vous pouvez coder autrement.
+
+L'arbre initial est créé vide - sa racine est donc le pointeur nul (nullptr).
+
+Implémenter la méthode insert(int v) qui ajoute une valeur entière à l'arbre à la bonne place i.e.
+  - on va se déplacer dans l'arbre jusqu'à trouver l'endroit où insérer cette valeur
+  - si la valeur existe déjà dans l'arbre, on ne fait rien
+  - sinon on crée un noeud qu'on raccorde.
+
+  Réfléchissez à la manière de faire un tel parcours récursivement, soit codez-le soit regardez les indices ci-dessous.
+
+Indices pour l'insertion dans l'arbre binaire:
+  - si la racine n'existe pas on crée un noeud avec la valeur et ses deux sous-arbres nuls (nullptr)
+  - si la racine existe on lance l'insertion sur ce noeud racine (on appelle une méthode d'insertion des noeuds)
+
+Indices pour la méthode d'insertion sur un noeud:
+  - si la valeur du noeud est égale à la valeur à insérer on ne fait rien (le noeud y est déjà)
+  - si la valeur est inférieure et si l'arbre gauche existe on lance l'insertion sur le noeud gauche
+  -                             et si l'arbre gauche n'existe pas, on crée un noeud avec la valeur et il devient le fils gauche
+  - pareil à droite
+
+II) Parcours de l'arbre:
+
+Implémentez la méthode depth() qui va parcourir cet arbre en profondeur en affichant ses valeurs.
+Réfléchissez à la manière de faire ce parcours de manière récursive et soit vous le codez soit vous lisez les indices ci-dessous.
+
+Indices pour un parcours en profondeur récursif d'un arbre binaire:
+   - ce parcours lance le parcours récursif du noeud racine de l'arbre (i.e. appelle la méthode depth des noeuds)
+   - si ce noeud a un fils gauche, on lance le parcours sur le noeud gauche (i.e. appelle la méthode sur le noeud gauche)
+   - si ce noeud n'a pas de fils gauche, on affiche la valeur du noeud et on lance le parcours sur le noeud droit
+...
+
+Lors de ce parcours on peut afficher la valeur des noeuds à plusieurs endroits:
+   - si on l'affiche en début de l'appel du parcours sur un noeud: on a un affichage dit préfixe
+   - si on l'affiche entre les appels à gauche et à droite on a un affichage dit infixe
+   - si on l'affiche à la fin de l'appel de la méthode sur un noeud: on a un affichage dit postfixe
+
+Pour décider de l'affichage, on peut donc passer à cette méthode le type de l'affichage désiré
+(par exemple un objet qui est une énumération (enum) voir exemple ci-dessous)
+
+*/
+#include <iostream>
+
+// the type enum
+enum Order
+{
+     Prefix,  // will be set to 0
+     Infix,   // will be set to the previous + 1
+     Postfix, // idem
+};
+
+void enum_values()
+{
+     std::cout << "Prefix is " << Order::Prefix << std::endl;
+     std::cout << "Infix is " << Order::Infix << std::endl;
+     std::cout << "Postfix is " << Order::Postfix << std::endl;
+}
+
+// Les structures de base de l'arbre binaire BinaryTree et Node:
+// avec les méthodes pour que le code compile.
+struct Node
+{
+     int value;
+     Node* Left;
+     Node* Right;
+
+     Node(int v):value(v),Left(nullptr),Right(nullptr){}
+
+     void insert(int v){
+          if (value>v){
+               if(Left==nullptr){
+                    Left=new Node(v);
+               }
+               else {
+                    Left->insert(v);
+               }
+          }
+          else if(value<v){
+               if(Right==nullptr){
+                    Right=new Node(v);
+               }
+               else{
+                    Right->insert(v);
+               }
+          }
+     }
+
+     bool search(int v){
+          if(value==v){
+               return true;
+          }
+          else if(value>v){
+               if(Left==nullptr){
+                    return false;
+               }
+               else {
+                    return Left->search(v);
+               }
+            }
+          else{
+               if(Right==nullptr){
+                    return false;
+               }
+               else{
+                    return Right->search(v);
+               }
+          }
+     }
+     void depth(Order o){
+        if (o == Prefix){
+          std::cout << value << " ";
+          }
+          if (Left!=nullptr){
+           Left->depth(o);
+        }
+        if (o == Infix){
+          std::cout << value << " ";
+        }
+        if (Right!=nullptr){
+          Right->depth(o);
+        }
+        if (o == Postfix){
+          std::cout << value << " ";
+        }
+     }
+     void print(int i=0){
+        if (Left!=nullptr){
+            Left->print(i+5);
+        }
+        if (Right!=nullptr){
+            Right->print(i+5);
+        }
+        std::cout<<std::string(i,' ')<<value<<std::endl;
+     }
+
+};
+
+struct BinaryTree
+{
+     Node* root=nullptr;
+     void insert(int v) {
+          if(root==nullptr){
+               root= new Node(v);
+          }
+          else {
+               root->insert(v);
+          }
+     }
+     void depth(Order o) {
+          if(root!=nullptr){
+               std::cout<<'{';
+               root->depth(o);
+               std::cout<<'}'<<std::endl;
+          }
+     }
+     bool search(int v) {
+          if(root==nullptr){
+               return false;
+          }
+          else{
+               return root->search(v);
+          }
+        }
+     void print() {
+          if(root!=nullptr){
+               return root->print();
+          }
+     }
+};
+
+int main()
+{
+      //     enum_values(); // exemple pour voir un enum
+
+     // on construit l'arbre
+
+     // cet ordre d'insertion donne un bel arbre binaire équilibré
+     // ce qui n'est bien sûr pas toujours le cas...
+     // il existe des algorithmes pour garder l'arbre équilibré
+     // mais c'est en dehors de notre cadre d'une simple programmation
+     BinaryTree b;
+     b.insert(8);
+     b.insert(4);
+     b.insert(6);
+     b.insert(12);
+     b.insert(10);
+     b.insert(11);
+     b.insert(9);
+     b.insert(2);
+     b.insert(1);
+     b.insert(3);
+     b.insert(5);
+     b.insert(14);
+     b.insert(15);
+     b.insert(13);
+     b.insert(7);
+     b.print();
+     
+    std::cout << std::boolalpha << b.search(14) << std::endl;
+
+     return 0;
+
+     std::cout << "Infix ";
+     b.depth(Order::Infix);
+     std::cout << "Postfix ";
+     b.depth(Order::Postfix);
+     std::cout << "Prefix ";
+     b.depth(Order::Prefix);
+     std::cout << std::boolalpha << b.search(25) << std::endl;
+     std::cout << std::boolalpha << b.search(2) << std::endl;
+
+     std::cout << "Cet arbre est dégénéré:\n";
+     BinaryTree bad1;
+     for (int i = 1; i < 16; i++)
+          bad1.insert(i);
+     bad1.print();
+
+     std::cout << "\ncelui là aussi:\n";
+     BinaryTree bad2;
+     for (int i = 30; i > 15; i--)
+          bad2.insert(i);
+     bad2.print();
+}
+
+/* l'exécution de ce programme donne:
+    ----------------------------------
+
+Prefix is 0
+Infix is 4
+Postfix is 5
+               15
+          14
+               13
+     12
+               11
+          10
+               9
+8
+               7
+          6
+               5
+     4
+               3
+          2
+               1
+Infix { 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15  }
+Postfix { 1 3 2 5 7 6 4 9 11 10 13 15 14 12 8  }
+Prefix { 8 4 2 1 3 6 5 7 12 10 9 11 14 13 15  }
+false
+true
+Cet arbre est dégénéré:
+                                                                      15
+                                                                 14
+                                                            13
+                                                       12
+                                                  11
+                                             10
+                                        9
+                                   8
+                              7
+                         6
+                    5
+               4
+          3
+     2
+1
+
+celui là aussi:
+30
+     29
+          28
+               27
+                    26
+                         25
+                              24
+                                   23
+                                        22
+                                             21
+                                                  20
+                                                       19
+                                                            18
+                                                                 17
+                                                                      16
+*/
