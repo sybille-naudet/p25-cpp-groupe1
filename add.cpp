e250419110eff9a712c03d04fdb67b062da54045
sybille <syb.ndt@gmail.com>
2026-02-22 15:18:35 +0100

    version finale add.cpp

--- add.cpp
+++ add.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
-#include <cstdlib>
 
-int main(int argc, char* argv[]) {
+int convertir(char str[]) {
+    int res = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        char a = str[i];
+        res = res * 10 + a - '0';
+    }
+    return res;
+}
 
+int add(int a, int b) {
+    return a + b;
+}
+
+
+int main(int argc, char *argv[]) {
     if (argc < 2) {
+        std::cout << "pas assez d'arguments" << std::endl;
+    }
+    else {
+        int somme = 0;
+        int i = 1;
+        while (i < argc) {
+            int nombre = convertir(argv[i]);
+            somme = add(somme, nombre);
+            i = i + 1;
+        }
 
-        std::cerr << "not enough arguments" << std::endl;
-        return -1;
+        std::cout << "résultat : " << somme << std::endl;
     }
-    int total = 0;
-    int i = 1;
-    while (i < argc) {
-        total = total + std::atoi(argv[i]);
-        i = i + 1;
-    }
-    std::cout << "résultat : " << total << std::endl;
     return 0;
- }
+}
