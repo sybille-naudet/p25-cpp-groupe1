ce2de837aa325953cd749bb6b23b5b52873c50d3
sybille <syb.ndt@gmail.com>
2026-02-22 15:37:30 +0100

    version finale stac-tp

--- stack-tp.cpp
+++ stack-tp.cpp
@@ -8,11 +8,10 @@
 //      (qui testent si la pile est resp. vide ou pleine)
 //      il faut les utiliser dans votre code de push et pop
 
-void print(int tab[],int taille, int nb)
+void print(int tab[],int taille, int nb) {
 // cette fonction affiche les entiers empilés
-{
     std::cout<<'[';
-    for(int i=0;i<nb;i++){
+    for(int i=0; i<nb; i++) {
         std::cout<<tab[i];
         if(i<nb-1){
             std::cout<<",";
@@ -22,8 +21,7 @@ void print(int tab[],int taille, int nb)
 }
 
 // renvoie true si la pile est vide, false sinon
-bool is_empty(int tab[], int taille, int nb)
-{
+bool is_empty(int tab[], int taille, int nb) {
     if(nb==0){return true;}
     else {return false;}
 }
@@ -35,36 +33,25 @@ bool is_full(int tab[], int taille,int nb)
     else {return false;}
 }
 
-void push(int tab[], int taille, int& nb,int valeur)
+void push(int tab[], int taille, int& nb,int valeur) {
 // cette fonction ajoute un entier à la pile passé en argument
 // elle doit s'assurer que la pile n'est pas pleine avant d'empiler
 // elle lance une exception si problème
-{
-    if(is_full(tab, taille, nb)) {
+    if (is_full(tab, taille, nb)) {
         throw std::runtime_error("liste pleine");
     }
     tab[nb]=valeur;
     nb = nb+1;
 }
 
-int top(int tab[],int taille, int nb) // (cette fonction ne dépile pas)
-// cette fonction retourne l'entier en haut de la pile (le dernier empilé)
-// à la sortie de cette fonction l'élément retourné reste dans la pile
-// elle doit s'assurer que la pile n'est pas vide avant de la dépiler
-// elle lance une exception si problème
-{
-    if(is_empty(tab,taille,nb)){
+int top(int tab[],int taille, int nb) {
+    if(is_empty(tab,taille,nb)) {
         throw std::runtime_error("liste vide");
     }
     return tab[nb-1];
 }
 
-int pop(int tab[],int taille, int& nb) // (cette fonction dépile)
-// cette fonction retourne l'entier en haut de la pile (le dernier empilé)
-// à la sortie de cette fonction l'élément retourné n'est plus compté dans la pile
-// elle doit s'assurer que la pile n'est pas vide avant de la dépiler
-// elle lance une exception si problème
-{
+int pop(int tab[],int taille, int& nb) {
     if(is_empty(tab,taille,nb)){
         throw std::runtime_error("erreur liste vide");
     }
@@ -87,47 +74,35 @@ int* create_stack(int size){
     return t;
 }
 
-void delete_stack(int *argv){
+void delete_stack(int* argv){
     delete[] argv;
 }
 
-int main()
-{
-
-    // on définit les 3 variables constituant la pile
+int main() {
     int taille = 5;
-    int* tab = create_stack(taille);
-    int stack[taille];
+    int* stack = create_stack(taille);
     int nb = 0;
 
-    push(tab, taille, nb, -17);
-    // rajoutez aux blocks catch les erreurs
-    // pouvant être déclenchées par les opérations sur la pile
-    try
-    {
-        print(stack, taille, nb); // affiche [ [
+    try {
+        print(stack, taille, nb); 
         push(stack, taille, nb, -17);
+        push(stack, taille, nb, 90);
+        print(stack, taille, nb);
+        std::cout << "Le sommet est : " << top(stack, taille, nb) << std::endl;
+        int valeur_sortie = pop(stack, taille, nb);
+        std::cout << "On a retire : " << valeur_sortie << std::endl;
+        print(stack, taille, nb);
 
-        // on affiche l'élément en haut de pile
-        std::cout << top(stack, taille, nb) << std::endl; // affiche -17
-
-        print(stack, taille, nb); // affiche [-17 90 [
-
-        int e = pop(stack, taille, nb);
-        std::cout << e << std::endl;
-        print(stack, taille, nb); // affiche [-17 [
-
+        //pour forcer l'erreur
+        push(stack, taille, nb, 10);
         push(stack, taille, nb, 20);
-        push(stack, taille, nb, -78);
-        push(stack, taille, nb, -9);
-        push(stack, taille, nb, -56);
-        push(stack, taille, nb, -9);
-        push(stack, taille, nb, 68);
+        push(stack, taille, nb, 30);
+        push(stack, taille, nb, 40);
+        
+        std::cout << "élément en trop" << std::endl;
+        push(stack, taille, nb, 50); //exception
     }
-    catch(const std::runtime_error& e)
-    {
-        std::cout<<"erreur"<<std::endl;
+    catch (const std::runtime_error& e) {
+        std::cout << "Erreur : " << e.what() << std::endl;
     }
 }
-   
-    
